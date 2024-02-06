b13424d4ecdafe5405f650257e2fe632acff8b63
rezaul118255 <rezaul118255@gmail.com>
2024-02-06 07:42:10 +0600

    project-start

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="container">
           <div className="wrapper">
+            {/* Navbar  */}
             <Navbar />
             {children}
             <Footer />
