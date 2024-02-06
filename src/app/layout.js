686db800b68bbd8e2aba7f21f4439c183c79d244
rezaul118255 <rezaul118255@gmail.com>
2024-02-06 07:52:08 +0600

    branch create

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
         <div className="container">
           <div className="wrapper">
             {/* Navbar  */}
+            {/* Navbar  */}
             <Navbar />
             {children}
             <Footer />
