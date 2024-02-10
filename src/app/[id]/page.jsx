9d0da248e61d17aab75735fb54a4549f9b9f65dc
rezaul118255 <rezaul118255@gmail.com>
2024-02-10 10:57:02 +0600

    blog page compleate

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,9 +1,47 @@
+import Comments from '@/components/comments/Comments';
+import Menu from '@/components/menu/Menu';
+import Image from 'next/image';
+import image from "../../../public/food.png"
 import React from 'react';
-
+import styles from "./singlePage.module.css";
 const page = () => {
     return (
         <div>
-            <h1>this is id page</h1>
+            <div className={styles.container}>
+                <div className={styles.infoContainer}>
+                    <div className={styles.textContainer}>
+                        <h1 className={styles.title}>Rezaul</h1>
+                        <div className={styles.user}>
+
+                            <div className={styles.userImageContainer}>
+                                <Image src={image} alt="" fill className={styles.avatar} />
+                            </div>
+
+                            <div className={styles.userTextContainer}>
+                                <span className={styles.username}> Rezaul</span>
+                                <span className={styles.date}>01.01.2024</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.imageContainer}>
+                        <Image src={image} alt="" fill className={styles.image} />
+                    </div>
+
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.post}>
+                        <div
+                            className={styles.description}
+
+                        />
+                        <div className={styles.comment}>
+                            <Comments />
+                        </div>
+                    </div>
+                    <Menu />
+                </div>
+            </div>
         </div>
     );
 };
