65ba6e8c945e1b8a31259806aaee2a7640513628
rezaul118255 <rezaul118255@gmail.com>
2024-02-10 09:55:52 +0600

    Footer compleate

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,15 @@ import styles from "./navbar.module.css";
 import Link from 'next/link';
 import ThemeToggle from '../themeToggle/ThemeToggle';
 import AuthLinks from '../authLinks/AuthLinks';
+import logo from "../../../public/logo_gif_anim4_small-d5d61787.gif"
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <div className={styles.logo} >
-                <h3>Achintya Blog</h3>
+                <Image width={50} height={50} class="h-[45px] w-[190px] max-md:h-[25px] max-md:w-[110px] cursor-pointer" src={logo} alt=""></Image>
+                {/* <h3>Achintya Blog</h3> */}
             </div>
             <div className={styles.links}>
 
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import styles from './pagination.module.css'
+"use client";
+
+import React from "react";
+import styles from "./pagination.module.css";
+
+
 const Pagination = () => {
+
     return (
         <div className={styles.container}>
-            <h1>Pagination</h1>
+            <button
+                className={styles.button}
+
+
+            >
+                Previous
+            </button>
+            <button
+
+                className={styles.button}
+
+            >
+                Next
+            </button>
         </div>
     );
 };
