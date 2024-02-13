565ad075259f3c529285cf36750d68b8f01025ef
rezaul118255 <rezaul118255@gmail.com>
2024-02-13 11:07:42 +0600

    project setup

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,22 +1,4 @@
-// import CardList from "@/components/cardList/CardList";
-// // import styles from "./blogPage.module.css";
-// import Menu from "@/components/Menu/Menu";
 
-// const BlogPage = () => {
-
-
-//     return (
-//         <div className={styles.container}>
-//             <h1 className={styles.title}> Blog</h1>
-//             <div className={styles.content}>
-//                 <CardList />
-//                 <Menu />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default BlogPage;
 import CardList from '@/components/cardList/CardList';
 import Menu from '@/components/menu/Menu';
 import styles from "./blogPage.module.css";
